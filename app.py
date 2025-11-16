395d6eb80fdeae2152f64c290c626a4ea93b965e
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-11-16 19:20:41 +0400

    test deploy

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
+import json
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+app.json.ensure_ascii = False
+
+with open("backup.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    # print(data)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+class Data(Resource):
+    def get(self):
+        return data
+
+
+api.add_resource(Data, "/data")
 
 
 if __name__ == "__main__":
